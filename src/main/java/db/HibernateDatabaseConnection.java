1f2f7b010cafa0456ae9ddea480e64ea73b3d420
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-11-07 14:03:58 +0000

    hibernate

--- src/main/java/db/HibernateDatabaseConnection.java
+++ src/main/java/db/HibernateDatabaseConnection.java
@@ -9,8 +9,30 @@ import org.hibernate.boot.MetadataSource;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-
+import com.dao.Student;
 
 public class HibernateDatabaseConnection {
-
+	private static StndardServiceRegistry registry;
+	private static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		if(sessionFactory == null)
+		{
+			try {
+				StandardServiceRegisterBuilder registeryBuilder = 
+						new StandardServiceRegisteryBuilder();
+				
+				
+				Map settings = new HashMap<>();				
+			}catch(Esception e)
+			{
+				
+			}
+		}
+	}
+	
+	
+	
+	
+	
 }
