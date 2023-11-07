b17b6e5750546db24bf368f84622ea55c375cd43
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-11-07 13:56:55 +0000

    hibernate

---
+++ src/main/java/db/HibernateDatabaseConnection.java
@@ -0,0 +1,16 @@
+package db;
+
+import java.utili.Hashmap;
+import java.util.Map;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSource;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+
+public class HibernateDatabaseConnection {
+
+}
